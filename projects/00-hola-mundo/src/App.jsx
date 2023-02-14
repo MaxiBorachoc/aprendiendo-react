02743c259a3d6a9cf2094025fa164bc4ea524899
MaxiBorachoc <maxi.borachoc@gmail.com>
2023-02-14 00:55:53 -0300

    endHolamundo

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,17 +1,45 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
-export function App(){
-     return (
-        <div className='App'>
-            <TwitterFollowCard  userName="midudev" initialIsFollowing={true}> 
-                Miguel Angel Duran
-            </TwitterFollowCard>
+const users = [
+    {
+        userName:"midudev",
+        name:"Miguel Ángel Durán",
+        isFollowing: true,
+    },
+    {
+        userName:"maxibora",
+        name:"Maxi",
+        isFollowing: false,
+    },
+    {
+        userName:"leomessi",
+        name:"Lionel Messi",
+        isFollowing: true,
+    },
+    {
+        userName:"angeldimaria",
+        name:"Angel Di Maria",
+        isFollowing: false,
+    }
+] 
 
-            <TwitterFollowCard  userName="maxibora">
-                Maxi
-            </TwitterFollowCard>
-         
+export function App(){
+    
+    
+    return (
+        <div className='App'>
+         {
+            users.map(({userName, name, isFollowing}) => (
+                <TwitterFollowCard
+                    key={userName}
+                    userName={userName}
+                    initialIsFollowing={isFollowing}
+                >
+                    {name}
+                </TwitterFollowCard>
+            ))
+         }
         </div>
      )
 }
