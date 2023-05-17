cfef352c6acc85d60f674d877e1c2f6ae7dd63e6
MaxiBorachoc <maxi.borachoc@gmail.com>
2023-05-16 21:17:03 -0300

    add services

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
-
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+import './App.css'
+import { getRandomFact } from './services/fact.js'
 
 //const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`
 
@@ -11,29 +11,39 @@ export function App(){
     const [imageUrl, setImageUrl] = useState()
     //fetching de datos
 
+   // para recuperar la cita al cargar la página
     useEffect(()=>{
-        fetch(CAT_ENDPOINT_RANDOM_FACT)
-            .then(res => res.json())
-            .then(data => {
-                const { fact } = data
-                setFact( fact )
-                const threeFirstWords = fact.split(' ', 3).join(' ')
-
-                fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
-                    .then(res => res.json())
-                    .then(response =>{
-                        const { url } = response
-                        setImageUrl(url)
-                    })
-            })
+        getRandomFact().then(newFact => setFact(newFact))
     }, [])
     //NO OLVIDAR EL [] PARA LAS DEPENDENCIAS!!
 
+    // para recuperar la imagen cada vez que tenemos una cita nueva
+
+    useEffect(()=>{
+        if (!fact) return
+
+        const threeFirstWords = fact.split(' ', 3).join(' ')
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+            .then(res => res.json())
+            .then(response =>{
+                const { url } = response
+                setImageUrl(url)
+            })
+    }, [fact])
+
+    const handleClick = async ()=>{
+        const newFact = await getRandomFact()
+        setFact(newFact)
+    }
+
+
     return(
-        <main style={{display: 'flex', flexDirection:'column'}}>
+        <main>
             <h1>App de gatitos</h1>
+            <button onClick={handleClick}>Get new fact</button>
             {fact && <p>{fact}</p>}
-            {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`} />}
+            {imageUrl && <img 
+            src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`} />}
             
         </main>
     )
